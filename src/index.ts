6f10b2bab26e8d280930350619fde13be5c6e948
the_dongle <stevewalkerbeta@gmail.com>
2024-09-14 15:39:30 +0100

    ..

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { build } from "./server.js";
 const server = build({ logger: process.env.NODE_ENV === "development" });
 
 server.listen(
-  { port: parseInt(`${process.env.port}`), host: process.env.host },
+  { port: 3000, host: process.env.host },
   function (err, address) {
     if (err) {
       server.log.error(err);
